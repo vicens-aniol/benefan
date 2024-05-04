232aa4d14dd11a446a252323e2430cb3d3d534d2
Aniol <aniol.vicens@gmail.com>
2024-05-04 14:05:45 +0200

    Packages update and remove nativewind

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,7 @@
 import { AppState } from 'react-native'
 import 'react-native-url-polyfill/auto'
 import { createClient } from '@supabase/supabase-js'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL ?? 'placeholder-public-url';
 const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY ?? 'placeholder-public-anon-key';
@@ -9,6 +10,7 @@ console.log("supabaseUrl: " + supabaseUrl + ", supabaseAnonKey: " + supabaseAnon
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
     auth: {
+        storage: AsyncStorage,
         autoRefreshToken: true,
         persistSession: true,
         detectSessionInUrl: false,
