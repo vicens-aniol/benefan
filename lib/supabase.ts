4def0592384cd27768f4cc32de9891cd85ac790d
uripont-upf <oriol.pont01@estudiant.upf.edu>
2024-05-04 11:23:53 +0200

    doctor fixed dependencies (let's see if it builds now)

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,5 @@
 import { AppState } from 'react-native'
 import 'react-native-url-polyfill/auto'
-import AsyncStorage from '@react-native-async-storage/async-storage'
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL ?? 'placeholder-public-url';
@@ -10,7 +9,6 @@ console.log("supabaseUrl: " + supabaseUrl + ", supabaseAnonKey: " + supabaseAnon
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
     auth: {
-        storage: AsyncStorage,
         autoRefreshToken: true,
         persistSession: true,
         detectSessionInUrl: false,
