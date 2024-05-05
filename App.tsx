1ef3dd5ebc050a019c22f366ed8a053db6630a0a
uripont-upf <oriol.pont01@estudiant.upf.edu>
2024-05-05 04:08:36 +0200

    why is it forgetting i have commited

---
+++ App.tsx
@@ -0,0 +1,128 @@
+import React, { useState, useEffect } from 'react';
+import { View, StyleSheet, Dimensions } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+
+
+import Home from './pages/Home';
+import Details from './pages/Details';
+import YourCelebrities from "./pages/YourCelebrities";
+import CarouselPage from './pages/CarouselPage';
+import SearchScreen from './pages/SearchScreen';
+import CelebrityInfo from './pages/CelebrityInfo';
+import ScheduleScreen from './pages/ScheduleScreen';
+import CountdownTimerPage from './pages/CountdownTimerPage';
+
+
+import Auth from './components/Auth';
+import { supabase } from './lib/supabase';
+import { Session } from '@supabase/supabase-js';
+
+// Get screen width for dynamic positioning
+const screenWidth = Dimensions.get('window').width;
+
+type RootStackParamList = {
+  Carousel: undefined;
+  Search: undefined;
+  CelebrityInfo: {
+    name: string;
+    role: string;
+    image: any; // This should be ImageSourcePropType if you are using TypeScript
+    date: string;
+    description: string;
+  };
+};
+
+const CarouselStack = createStackNavigator<RootStackParamList>();
+
+function CarouselStackScreen() {
+  return (
+    <CarouselStack.Navigator>
+      <CarouselStack.Screen name="Carousel" component={CarouselPage} options={{ headerShown: false, gestureEnabled:false}} />
+      <CarouselStack.Screen name="Search" component={SearchScreen} options={{ headerShown: false, gestureEnabled:false }} />
+      <CarouselStack.Screen name="CelebrityInfo" component={CelebrityInfo} options={{ headerShown: false }} />
+    </CarouselStack.Navigator>
+  );
+}
+
+const App: React.FC = () => {
+  const [session, setSession] = useState<Session | null>(null);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+  }, []);
+
+  const Tab = createBottomTabNavigator();
+
+  return (
+    session && session.user ? (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName = '';
+
+            if (route.name === 'Home') {
+              iconName = 'home';
+            } else if (route.name === 'Details') {
+              iconName = 'list';
+            } else if (route.name === 'Search') {
+              iconName = 'search';
+            }
+
+            return (
+              <View style={focused ? styles.selectedIcon : null}>
+                <FontAwesome name={iconName} size={size} color={focused ? 'white' : color} />
+              </View>
+            );
+          },
+          tabBarStyle: styles.tabBarStyle,
+          tabBarShowLabel: false, // Hide labels under the icons
+          tabBarActiveTintColor: 'white',
+          tabBarInactiveTintColor: 'gray',
+        })}
+      >
+        <Tab.Screen name="Home" component={CarouselStackScreen} options={{ headerShown: false}}  />
+        <Tab.Screen name="Search" component={SearchScreen} options={{ headerShown: false }} />
+        <Tab.Screen name="Details" component={CountdownTimerPage} options={{ headerShown: false }}  />
+      </Tab.Navigator>
+    </NavigationContainer>
+  ) : (
+    <Auth />
+  ));
+};
+
+const styles = StyleSheet.create({
+  tabBarStyle: {
+    backgroundColor: 'white',
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    height: 100,
+    borderTopLeftRadius: 40,
+    borderTopRightRadius: 30,
+    borderBottomLeftRadius: 0,
+    borderBottomRightRadius: 0,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.1,
+    shadowRadius: 3,
+    elevation: 4,
+  },
+  selectedIcon: {
+    backgroundColor: '#4D8D93', // Green color
+    padding: 10,
+    borderRadius: 30, // Ensures a perfect circle
+  },
+});
+
+export default App;
---
+++ pages/CarouselPage.tsx
@@ -0,0 +1,133 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Image, TouchableOpacity, ImageSourcePropType } from 'react-native';
+import BlobBackground from '../components/BlobBackground';
+import { useNavigation } from '@react-navigation/native';
+import { StackNavigationProp } from '@react-navigation/stack';
+import commonStyles from '../styles/commonStyles';
+import { Alert } from 'react-native'; // Make sure to import Alert
+import FontAwesome from 'react-native-vector-icons/FontAwesome'; // Import from vector icons
+import { supabase } from "../lib/supabase";
+
+
+// Function to display a confirmation dialog before logging out
+const confirmSignOut = () => {
+  Alert.alert(
+    'Logout Confirmation',
+    'Are you sure you want to log out?',
+    [
+      {
+        text: 'Cancel',
+        style: 'cancel',
+      },
+      {
+        text: 'Logout',
+        onPress: async () => {
+          console.log("Signing out");
+          await supabase.auth.signOut(); // Replace with your logout logic
+        },
+      },
+    ],
+    { cancelable: false }
+  );
+};
+
+// Define the types for the celebrity and screen state
+type Celebrity = {
+  id: string;
+  image: ImageSourcePropType;
+  name: string;
+  description: string;
+};
+
+type RootStackParamList = {
+  Carousel: undefined;
+  Search: undefined;
+  CelebrityInfo: undefined
+};
+
+type Screen = 'Carrousel1' | 'Carrousel2' | 'Carrousel3' | 'Search' | 'CelebrityInfo';
+
+// Sample data for the celebrities
+type CarrouselNavigationProp = StackNavigationProp<RootStackParamList, 'Carousel'>;
+const celebData: Celebrity[] = [
+  { id: '1', image: require('../assets/IMG/Celebrities/TaylorSwift.png'), name: 'Taylor Swift', description: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC.' },
+  { id: '2', image: require('../assets/IMG/Celebrities/LionelMessi.png'), name: 'Lionel Messi', description: 'Lorem Ipsum is not just random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.' },
+  { id: '3', image: require('../assets/IMG/Celebrities/Eminem.png'), name: 'Eminem', description: 'Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type.' }
+];
+
+const CarouselPage: React.FC = () => {
+  const [currentScreen, setCurrentScreen] = useState<Screen>('Carrousel1');
+  const navigation = useNavigation<CarrouselNavigationProp>();
+
+  const navigate = () => {
+    if (currentScreen === 'Carrousel3') {
+      navigation.navigate('Search');
+    } else {
+      const nextIndex = parseInt(currentScreen.slice(-1), 10) + 1;
+      setCurrentScreen(`Carrousel${nextIndex}` as Screen);
+    }
+  };
+
+  // Determine the index from the currentScreen for less redundancy
+  const index = parseInt(currentScreen.slice(-1), 10) - 1;
+  const celeb = celebData[index];
+
+  return (
+    <BlobBackground>
+      <View style={styles.container}>
+        <TouchableOpacity style={styles.logoutIcon} onPress={confirmSignOut}>
+          <FontAwesome name="power-off" size={27} color="#666" />
+        </TouchableOpacity>
+        <Image source={celeb.image} style={styles.image} resizeMode="cover" />
+        <Text style={styles.header}>{celeb.name}</Text>
+        <Text style={styles.description}>{celeb.description}</Text>
+        <TouchableOpacity style={commonStyles.buttonContainer_rounded} onPress={navigate}>
+          <Text style={commonStyles.boldlabelforbutton}>Next</Text>
+        </TouchableOpacity>
+      </View>
+    </BlobBackground>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+    backgroundColor: 'transparent'
+  },
+  image: {
+    width: 300,
+    height: 400,
+    borderRadius: 20,
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 20,
+  },
+  description: {
+    fontSize: 16,
+    color: '#666',
+    textAlign: 'left',
+    marginTop: 10,
+    marginBottom: 20,
+  },
+  buttonContainer: {
+    backgroundColor: '#4D8D93',
+    borderRadius: 6,
+    paddingVertical: 10,
+    paddingHorizontal: 100,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logoutIcon: {
+    position: 'absolute',
+    top: 60, // Adjust to fit your layout
+    left: 20, // Adjust to fit your layout
+    color: '#fff',
+  },
+});
+
+export default CarouselPage;
